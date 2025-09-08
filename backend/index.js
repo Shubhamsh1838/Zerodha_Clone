32bf4b26736f95eae9042e08e04c8f5c92450dea
Shubhamsh1838 <23m1838@iitb.ac.in>
2025-09-08 19:32:33 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
+const authRoute = require('./routes/AuthRoutes');
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
+const app = express();
+
+
+app.use(cors({
+  origin: [process.env.Dashboard_url, process.env.Frontend_url ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+    console.log("App started!");
+    mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+   .then(() => console.log("MongoDB is  connected successfully"))
+   .catch((err) => console.error(err));
+});
+app.use("/", authRoute);
+
---
+++ backend/routes/AuthRoutes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+
+const { Signup, Login } = require("../Controllers/AuthController");
+const {userVerification} = require("../middlewares/AuthMiddleware");
+const {HoldingsModel} = require("../model/HoldingsModel");
+const {PositionsModel} = require("../model/PositionsModel");
+const {OrdersModel} = require("../model/OrdersModel");
+
+router.get("/allHoldings", async(req, res)=>{
+    let allHoldings = await HoldingsModel.find({});
+    res.json(allHoldings);
+});
+
+router.get("/allPositions", async(req, res)=>{
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+});
+
+router.post("/newOrder", async(req,res) => {
+    let newOrder = new OrdersModel({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+
+    newOrder.save();
+    res.send("order saved!");
+})
+router.get("/allOrders", async(req,res)=> {
+    let allOrder = await OrdersModel.find({});
+    res.json(allOrder);
+});
+
+router.post("/",userVerification);
+
+router.post("/signup", Signup);
+
+router.post("/login", Login);
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  return res.json({ success: true, message: "Logged out successfully" });
+});
+
+
+module.exports = router;
