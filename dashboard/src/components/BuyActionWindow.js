50030bcda0dd3442c2d1dec89ad106aba8c676df
Shubhamsh1838 <23m1838@iitb.ac.in>
2025-09-10 19:22:14 +0530

    Update BuyActionWindow.js

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -11,7 +11,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodha-clone-server.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
@@ -67,5 +67,6 @@ const BuyActionWindow = ({ uid }) => {
     </div>
   );
 };
+
 
 export default BuyActionWindow;
